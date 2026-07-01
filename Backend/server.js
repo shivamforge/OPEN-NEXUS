d7637f81a343091018ac74e9e591158832cf604b
shivamforge <shivamk.it.25@nitj.ac.in>
2026-07-01 15:17:27 +0530

    Use Gmail SMTP host and port

--- Backend/server.js
+++ Backend/server.js
@@ -17,11 +17,17 @@ app.use(express.json());
 
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-       user: process.env.EMAIL_USER,
-       pass: process.env.EMAIL_PASS
-}});
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 app.post("/contact", async (req, res) => {
     console.log("REQUEST RECEIVED", Date.now());
     const { name, email, message } = req.body;
